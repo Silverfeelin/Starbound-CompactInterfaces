508f308372a3945b9f898ad23f4e412d689996c4
Silverfeelin <Silverfeelin@gmail.com>
2017-07-23 12:05:38 +0200

    Some comments

--- interface/colorPicker/colorPickerUtil.lua
+++ interface/colorPicker/colorPickerUtil.lua
@@ -1,18 +1,23 @@
 local positionKey = "colorPicker.position"
 local colorKey = "colorPicker.color"
 
+-- See setSerializedPosition.
 function getSerializedPosition()
   return status.statusProperty(positionKey)
 end
 
+-- The serialized position is used to show the user the selected color.
+-- This prevents us from having to know the position of each selectable color.
 function setSerializedPosition(pos)
   status.setStatusProperty(positionKey, pos)
 end
 
+-- {r,g,b} [0-255] or nil.
 function getSerializedcolor()
   return status.statusProperty(colorKey)
 end
 
+-- {r,g,b} [0-255] or nil.
 function setSerializedColor(color)
   status.setStatusProperty(colorKey, color)
 end
